62ccf54a71db2c25abd1b84d2d98fdbd07981198
adk2004 <dhingraaksh@gmail.com>
2025-03-13 21:43:20 +0530

    add loading spinner to Forgot Password and Login components

--- frontend/src/components/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react";
+import Loader from "./Loader.jsx";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log("Email: ", email);
-    const response = await fetch(
-      "http://localhost:5000/api/v1/users/forgot-password",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email }),
-      }
-    );
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/v1/users/forgot-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
+      await response.json();
+    } catch (error) {
+       alert(error.message);
+      console.log("Error: ", error);
+    } finally {
+      setLoading(false);
+    }
     // console.log(response);
-    const data = await response.json();
-    console.log(data);
   };
   return (
     <div className="inter flex flex-col items-center h-screen bg-[#0D1117] text-white py-12">
       <div className="py-4 text-2xl">Reset your password</div>
+      {loading && <Loader />}
       <div>
         <form
           onSubmit={handleSubmit}
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Loader from "./Loader.jsx";
 import axios from "axios";
 import { BACKEND_URI } from "../utils/constants.js";
 
 function Signin() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     customInput: "",
     password: "",
@@ -13,18 +15,28 @@ function Signin() {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(input);
   };
-  const userInput = isEmail(formData.customInput) ? { email: formData.customInput } : { username: formData.customInput };
+  const userInput = isEmail(formData.customInput)
+    ? { email: formData.customInput }
+    : { username: formData.customInput };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      const response = await axios.post(`${BACKEND_URI}/users/login`, {
-        ...userInput,
-        password: formData.password,
-      },{ withCredentials: true });
+      const response = await axios.post(
+        `${BACKEND_URI}/users/login`,
+        {
+          ...userInput,
+          password: formData.password,
+        },
+        { withCredentials: true }
+      );
       console.log("User logged in successfully", response.data);
       navigate("/");
     } catch (error) {
+      alert(error.message);
       console.log("Error: ", error);
+    } finally {
+      setLoading(false);
     }
   };
   const handleGoogleLogin = async (e) => {
@@ -35,37 +47,64 @@ function Signin() {
   return (
     <div className="inter flex flex-col items-center h-screen bg-[#0D1117] text-white py-12">
       <div className="py-4 text-2xl">Sign in to CodeSync</div>
+      {loading && <Loader />}
       <div>
-        <form onSubmit={handleSubmit} className="bg-[#151B23] w-[250px] py-4 px-3 border border-gray-700 rounded-sm">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-[#151B23] w-[250px] py-4 px-3 border border-gray-700 rounded-sm"
+        >
           <div className="flex flex-col mb-4">
             <label className="text-[12px]">Username or email address</label>
             <input
               type="text"
               value={formData.customInput}
               className="mt-2 py-1 px-2 text-[12px] bg-[#0D1117] border border-gray-700 rounded-sm"
-              onChange={(e) => setFormData({ ...formData, customInput: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, customInput: e.target.value })
+              }
               required
             />
           </div>
           <div className="flex flex-col mb-4">
-            <label className="text-[12px] flex justify-between">Password <Link to="/forgot"><span className="text-[10px] text-[#4493F8] cursor-pointer">Forgot password?</span></Link></label>
+            <label className="text-[12px] flex justify-between">
+              Password{" "}
+              <Link to="/forgot">
+                <span className="text-[10px] text-[#4493F8] cursor-pointer">
+                  Forgot password?
+                </span>
+              </Link>
+            </label>
             <input
               type="password"
               value={formData.password}
               className="mt-2 py-1 px-2 text-[12px] bg-[#0D1117] border border-gray-700 rounded-sm"
-              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
               required
             />
           </div>
-          <button className="cursor-pointer w-full text-sm bg-[#29903B] py-1 text-white rounded-sm font-semibold">Sign in</button>
+          <button className="cursor-pointer w-full text-sm bg-[#29903B] py-1 text-white rounded-sm font-semibold">
+            Sign in
+          </button>
         </form>
       </div>
       <div className="bg-[#151B23] w-[250px] py-3 px-3 border border-gray-700 rounded-sm mt-4 text-center">
-        <button onClick={handleGoogleLogin} className="text-sm text-[#4493F8] cursor-pointer">Sign in with Google</button>
-        <div className="text-[12px] mt-1">New to CodeSync? <Link to="/signup"><span className="text-[#4493F8]">Create an accont</span></Link></div>
+        <button
+          onClick={handleGoogleLogin}
+          className="text-sm text-[#4493F8] cursor-pointer"
+        >
+          Sign in with Google
+        </button>
+        <div className="text-[12px] mt-1">
+          New to CodeSync?{" "}
+          <Link to="/signup">
+            <span className="text-[#4493F8]">Create an accont</span>
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
